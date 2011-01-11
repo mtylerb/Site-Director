7def90ea68bada50a9c6ef2ca679f5b8d7b5fb25
Tyler Beckett <tyler@tbeckett.net>
2011-01-11 14:46:25 -0700

    Added readme with filler text.

--- views/index.php
+++ views/index.php
@@ -2,6 +2,6 @@
 <p>I've got a lovely bunch of coconuts.</p>
 <p>Here they are a standin' in a row.</p>
 <p>Big ones, small ones, some the size of yer head!</p>
-<p>Give 'em a twist, a flick of the wrist</p>
+<p>Give 'em a twist, a flick of the wrist,</p>
 <p>That's what the showman said.</p>
 
